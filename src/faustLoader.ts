5319e4530b7db9df943c196af2f134e125e380f5
Mason Bourgeois <mason.bourgeois@gmail.com>
2021-06-17 16:20:59 -0500

    do everything in isolated dirs to permit messy rebuilds

--- src/faustLoader.ts
+++ src/faustLoader.ts
@@ -6,6 +6,7 @@ import os from "os";
 import { exec as execCallback } from "child_process";
 const exec = util.promisify(execCallback);
 import fs from "fs-extra";
+import tmp from "tmp-promise";
 
 interface Options {
   outputPath?: string;
@@ -18,6 +19,8 @@ const faustLoader: LoaderDefinitionFunction<Options> = async function (
   const options: Options = getOptions(this);
   const { outputPath = "", publicPath = "/" } = options;
   const context = this.rootContext;
+  const workDir = await tmp.dir();
+
   const faust2wasmPath = await new Promise<string>((res) => {
     this.resolve(context, "faust-loader", (err, result) => {
       if (err) throw err;
@@ -27,23 +30,23 @@ const faustLoader: LoaderDefinitionFunction<Options> = async function (
       res(path.resolve(result, "../../faust2appls"));
     });
   });
-  const workDirPath = path.resolve(os.tmpdir(), "faust2appls");
 
-  await fs.copy(faust2wasmPath, workDirPath, {
-    overwrite: false,
-  });
+  await fs.copy(faust2wasmPath, path.resolve(workDir.path, "faust2appls"));
 
   const dspName = interpolateName(this, "[name]", { content, context });
-  const dspPath = path.resolve(workDirPath, dspName);
+  const dspPath = path.resolve(workDir.path, dspName);
 
   await fs.writeFile(dspPath, content);
-  const { stderr } = await exec(`./faust2wasm -worklet ${dspPath}`, {
-    cwd: workDirPath,
-  });
+  const { stderr } = await exec(
+    `./faust2appls/faust2wasm -worklet ${dspPath}`,
+    {
+      cwd: workDir.path,
+    }
+  );
   if (stderr) this.emitError(new Error(stderr));
 
   const wasmName = interpolateName(this, "[name].wasm", { context, content });
-  const wasmPath = path.resolve(workDirPath, `${dspName}.wasm`);
+  const wasmPath = path.resolve(workDir.path, wasmName);
   const wasmContent = await fs.readFile(wasmPath);
   // TODO: this method should accept a buffer
   // PR: https://github.com/webpack/webpack/pull/13577
@@ -53,16 +56,9 @@ const faustLoader: LoaderDefinitionFunction<Options> = async function (
     context,
     content,
   });
-  const processorPath = path.resolve(workDirPath, `${dspName}-processor.js`);
+  const processorPath = path.resolve(workDir.path, processorName);
   const processorContent = await fs.readFile(processorPath);
   this.emitFile(path.join(outputPath, processorName), processorContent);
-
-  await Promise.all([
-    fs.remove(dspPath),
-    fs.remove(path.resolve(workDirPath, `${dspName}.js`)),
-    fs.remove(wasmPath),
-    fs.remove(processorPath),
-  ]);
 
   const importPath = await new Promise((res) => {
     this.resolve(context, "faust-loader", (err, result) => {
