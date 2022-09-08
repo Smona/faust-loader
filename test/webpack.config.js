783955d7fac06fb328c089862da1570d7ff6a9cc
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-09-07 20:13:04 -0500

    Migrate test to typescript

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "index.js"),
+  entry: path.resolve(__dirname, "index.ts"),
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "my-first-webpack.bundle.js",
@@ -17,6 +17,16 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
       {
         test: /\.dsp$/,
         use: [
